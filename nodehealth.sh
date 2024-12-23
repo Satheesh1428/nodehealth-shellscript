436aa6bc52672a297895b016e3e44b614cd48b87
ASatheesh <satheeshovsn@gmail.com>
2024-12-23 16:32:07 +0530

    sh-cmt3

--- nodehealth.sh
+++ nodehealth.sh
@@ -3,6 +3,13 @@
 # Script : this script will check node health
 ###########################################################################################
 set -x  # debug mode
+# print the disk space
 df -h
+# Print the memory
 free -g
+# print the cpu
 nproc
+# to get running processer details
+
+ps -ef
+
