927cf0d766e60e8b9fabc1bf3257ea1e383c256f
ASatheesh <satheeshovsn@gmail.com>
2024-12-24 11:55:02 +0530

    sh-pgm1

--- Shellscriptpgm1.sh
+++ Shellscriptpgm1.sh
@@ -1,6 +1,6 @@
 for i in {1..100}; do
-if ([ 'expr $i % 3' == 0 ] || [ 'expr $i % 5' == 0 ]) && [ 'expr $i % 15' != 0 ];
+if ( [ $((i % 3)) -eq 0 ]  || [ $((i % 5)) == 0 ] ) && ( [ $((i % 15)) -ne 0 ] );
 then
-     echo $i
+     echo "$i"
 fi;
 done
